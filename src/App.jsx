ee5e42b601cb07f8a4d6fe542764b4c11bf1594f
danapc11 <danapc06@gmail.com>
2024-04-16 15:24:44 -0700

    Create SearchBar code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import {useState} from 'react'
+import SearchBar from './components/SearchBar'
+import ImageList from './components/ImageList'
+import SearchImage from './api'
 import './App.css'
 
 function App() {
+  const [images, setImages] =useState([])
+
+  const handleSubmit=async(term)=>{
+  console.log('uste está buscando con:', term)
+  }
+
   return (
     <div>
-      <h1>Hello There</h1>
+      <h1>App</h1>
+      <SearchBar onSubmit={handleSubmit}/>
     </div>
   )
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,26 @@
-function SearchBar(){
+import { useState } from "react"
+
+function SearchBar({onSubmit}){
+    const [term, setTerm]= useState('')
+
+    const handleFormSubmit=(event)=>{
+        event.preventDefault()
+        console.log('Necesito decirle al componente apá sobre los datos')
+        onSubmit(term)
+    }
+
+    const handlechange=(event)=>{
+        console.log(event.target.value)
+    }
+
     return(
         <div>
             <h1>Search Bar:</h1>
+            <form onSubmit={handleFormSubmit}>
+            confirma tu busqueda: {term}
+            <input onChange={handlechange} value={term}/>
+            </form>
+            <button onClick={handleClick}>Clik me!</button>
         </div>
     )
 }
